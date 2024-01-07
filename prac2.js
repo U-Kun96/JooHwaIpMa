e12594e8bace9e0bc9fb7c4bf103e7eae6e104fe
Minsik Yu <soule125@naver.com>
2024-01-07 22:03:27 +0900

    개인과제 파일 이름 변경

--- prac2.js
+++
@@ -1,9 +0,0 @@
-const api_Key = 'lhOpwn3M7x6UQcKwJ8IHuRDop80ZLquctsZSm6k0DbLBkeRNZ%2B6cijzQb7JjdeuL%2B%2F%2BW%2B2tA9mMi%2BoNgsdkKLw%3D%3D';
-async function getData() {
-    const url = `apis.data.go.kr/B552061/frequentzoneBicycle/getRestFrequentzoneBicycle?ServiceKey =${api_Key}&searchYearCd=2015&siDo=11&guGun=680&type=json&numOfRows=10&pageNo=1`;
-    const response = await fetch(url); 
-    const data = response.json();
-    
-    console.log("data", data);
-}
-getData();
